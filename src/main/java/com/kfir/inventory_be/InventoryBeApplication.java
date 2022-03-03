5a05cea53ff88b7e9ab0be874dd23bcd160b0be0
kfir <kfir19@gmail.com>
2022-03-03 10:28:22 +0200

    initial commit

---
+++ src/main/java/com/kfir/inventory_be/InventoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.kfir.inventory_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryBeApplication.class, args);
+	}
+
+}
